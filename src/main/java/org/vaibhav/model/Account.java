f48b422495fbee4eaccd7fa52666f23845dc42ee
Vaibhav <vaibhavpatidar0079@gmail.com>
2026-07-24 13:59:59 +0530

    feat: add Account

--- src/main/java/org/vaibhav/model/Account.java
+++ src/main/java/org/vaibhav/model/Account.java
@@ -5,14 +5,40 @@ public class Account{
     private String holderName;
     private double balance;
 
-    public void deposit(int amount){
+    public void deposit(double amount){
         if(amount >0) this.balance += amount;
-        else{throw new IllegalArgumentException();}
+        else{throw new IllegalArgumentException("Deposit amount must be positive.");}
     }
 
-    public void withdraw(int amount){
-        if(amount >0) this.balance += amount;
-        else{throw new IllegalArgumentException();}
+    public void withdraw(double amount){
+        if(amount > 0){
+            if(this.balance < amount) throw new IllegalArgumentException("Insufficient balance.");
+            this.balance -= amount;
+        }
+        else{throw new IllegalArgumentException("Withraw ammount cant be negative");}
+    }
+
+    public double getBalance(){
+        return this.balance;
+    }
+
+    public void setAccountNumber(int number) {
+        if (this.accountNumber != 0) {
+            throw new IllegalStateException("Account number is already assigned.");
+        }
+
+        this.accountNumber = number;
+    }
+
+    public int getAccountNumber(){
+        return accountNumber;
+    }
+
+    public void setHolderName(String name){
+        this.holderName = name;
+    }
+    public String getHolderName(){
+        return this.holderName;
     }
 
 }
